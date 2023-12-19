99c70d9b1c0c0c6f0570f8a6b72eba0462763b94
unknown <yayoub2017@gmail.com>
2023-12-19 10:27:49 +0100

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,27 @@
+import { ThemeProvider } from "@providers/theme-provider";
 import "./globals.css";
+import { Inter } from 'next/font/google'
 
-import { Footer, NavBar } from "@components";
 
 export const metadata = {
   title: "Car Hub",
   description: "Discover world's best car showcase application",
 };
 
+const inter = Inter({ subsets: ['latin'] })
+
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className='relative'>
-        <NavBar />
+      <body className={inter.className}>
+      <ThemeProvider 
+        attribute="class" 
+        defaultTheme="system" 
+        enableSystem
+      >
         {children}
-        <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,52 +1,90 @@
-import Image from "next/image";
-import Link from "next/link";
+import { Facebook, Instagram, Youtube } from 'iconsax-react'
+import Image from 'next/image'
+import React from 'react'
+import Logo from "../public/Morocco.png"
+import { useRouter } from 'next/navigation'
 
-import { footerLinks } from "@constants";
 
-const Footer = () => (
-  <footer className='flex flex-col text-black-100  mt-5 border-t border-gray-100'>
-    <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
-      <div className='flex flex-col justify-start items-start gap-6'>
-        <Image src='/logo.svg' alt='logo' width={118} height={18} className='object-contain' />
-        <p className='text-base text-gray-700'>
-          Carhub 2023 <br />
-          All Rights Reserved &copy;
-        </p>
-      </div>
+export const Footer = () => {
 
-      <div className="footer__links">
-        {footerLinks.map((item) => (
-          <div key={item.title} className="footer__link">
-            <h3 className="font-bold">{item.title}</h3>
-            <div className="flex flex-col gap-5">
-              {item.links.map((link) => (
-                <Link
-                  key={link.title}
-                  href={link.url}
-                  className="text-gray-500"
-                >
-                  {link.title}
-                </Link>
-              ))}
+    const router = useRouter();
+  return (
+
+    <section className="pb-6 bg-Mblack">
+        <div className="container">
+            <div className="mb-10 gap-y-8 row">
+                <div className="col-md-3">
+                    <Image width={100} height={100} quality={100} src={Logo} className='w-[150px] h-[35px] mb-2' alt="" />
+                    <p className="text__16 text-[#D4D4D4] mb-4">Revêtements céramiques pour voitures, avions, bâteaux, vitrages</p>
+                    <div className="flex items-center gap-3">
+                        <a href="#!" className='border border-Mgreen w-10 h-10 flex justify-center items-center rounded-full'>
+                        <Facebook
+                            size="18"
+                            color="#FFF"
+                            variant="Bold"
+                            />
+                        </a>
+                        <a href="#!" className='border border-Mgreen w-10 h-10 flex justify-center items-center rounded-full'>
+                        <Instagram
+                            size="18"
+                            color="#FFF"
+                            />
+                        </a>
+                        <a href="#!" className='border border-Mgreen w-10 h-10 flex justify-center items-center rounded-full'>
+                            <Youtube
+                            size="18"
+                            color="#FFF"
+                            variant="Bold"
+                            />
+                        </a>
+
+                    </div>
+                </div>
+                <div className="col-12 col-md-2 col-sm-6 offset-md-1">
+                    <h5 className="font-medium text-[#A3A3A3] text__16 mb-3">NAVIGATION</h5>
+                    <div className="flex flex-wrap gap-3 font-medium text__16">
+                        <a className="inline-block w-full text-Mwhite" href="/">ACCUEIL</a>
+                        <a className="inline-block w-full text-Mwhite" href="/about">EN SAVOIR PLUS</a>
+                        <a className="inline-block w-full text-Mwhite" href="/Automobile">AUTOMOBILE</a>
+                        <a className="inline-block w-full text-Mwhite" href="/Aeronautique">AÉRONAUTOQUE</a>
+                        <a className="inline-block w-full text-Mwhite" href="/Marine">MARINE</a>
+                    </div>
+                </div>
+                <div className="col-12 col-md-2 col-sm-6">
+                    <h5 className="font-medium text-[#A3A3A3] text__16 mb-3">MENU</h5>
+                    <div className="flex flex-wrap gap-3 font-medium text__16">
+                        <a className="inline-block w-full text-Mwhite" href="/blog">En savoir plus</a>
+                        <a className="inline-block w-full text-Mwhite" href="/boutique">Boutique</a>
+                        <a className="inline-block w-full text-Mwhite" href="/Partnaire">Devnire partenaire</a>
+                        <a className="inline-block w-full text-Mwhite" href="/contact">Contactez-vous</a>
+                    </div>
+                </div>
+                <div className="col-12 col-md-2 col-sm-6">
+                    <h5 className="font-medium text-[#A3A3A3] text__16 mb-3">INFORMATIONS</h5>
+                    <div className="flex flex-wrap gap-3 font-medium text__16">
+                        <a className="inline-block w-full text-Mwhite">42 Rue Georges Besse, 30000 Nîmes, France</a>
+                        <a className="inline-block w-full text-Mwhite">info@fusiocoat.com</a>
+                    </div>
+                </div>
+                <div className="col-12 col-md-2 col-sm-6">
+                    <h5 className="font-medium text-[#A3A3A3] text__16 mb-3">NOS PRODUITS</h5>
+                    <div className="flex flex-wrap gap-3 font-medium text__16">
+                        <a className="inline-block w-full text-Mwhite" href="/privacy">FC-ONE ULTIMATE</a>
+                        <a className="inline-block w-full text-Mwhite" href="/terms">FC-ONE SMART</a>
+                        <a className="inline-block w-full text-Mwhite" href="/terms">FC-BOOST</a>
+                        <a className="inline-block w-full text-Mwhite" href="/terms">FC-GLASS</a>
+                        <a className="inline-block w-full text-Mwhite" href="/terms">FC-TRIM</a>
+                    </div>
+                </div>
             </div>
-          </div>
-        ))}
-      </div>
-    </div>
-
-    <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
-      <p>@2023 CarHub. All rights reserved</p>
-
-      <div className="footer__copyrights-link">
-        <Link href="/" className="text-gray-500">
-          Privacy & Policy
-        </Link>
-        <Link href="/" className="text-gray-500">
-          Terms & Condition
-        </Link>
-      </div>
-    </div>
-  </footer>
-);
-
-export default Footer;
+            <div className="flex flex-wrap sm:flex-nowrap justify-center sm:justify-between gap-y-2 items-center px-4 py-2 bg-[#262626] rounded-full">
+                <p className="text-center text__14 text-[#A3A3A3] sm:!order-1 order-2">© 2023 FUSIOCOAT® WHD AGENCY.</p>
+                <div className="flex items-center justify-center sm:justify-end gap-6 order-1 sm:!order-2">
+                    <a className="inline-block sm:text__16 text__14 text-Mwhite flex-shrink-0" href="/Legal">Mentions légales</a>
+                    <a className="inline-block sm:text__16  text__14 text-Mwhite flex-shrink-0" href="/terms">Conditions générales de vente</a>
+                </div>
+            </div>
+       </div>
+   </section>
+  )
+}
